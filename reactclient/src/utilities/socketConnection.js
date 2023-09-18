d917448ac14bc2bba235847ea264766685252865
vladlisovkyi <vlad.lisovskyi.dev@gmail.com>
2023-09-18 16:58:36 +0300

    update soket url in react

--- reactclient/src/utilities/socketConnection.js
+++ reactclient/src/utilities/socketConnection.js
@@ -1,9 +1,4 @@
 import io from "socket.io-client";
-const socket = io("https://socket-io-7gf6.onrender.com", {
-  withCredentials: false,
-  extraHeaders: {
-    "my-custom-header": "abcd",
-  },
-});
+const socket = io.connect("https://socket-io-7gf6.onrender.com");
 socket.emit("clientAuth", "uihjt3refvdsadf");
 export default socket;
